1c6ed465fd195ff6bd4feb5edb3be72f35099cba
Thiago Oliveira de Paula <thiago-oliveira2001@live.com>
2020-09-19 23:57:19 -0300

    eslint fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ import { Main, Section, Sticky } from './styles';
 import FirstAndSecond from './components/FirstAndSecond';
 import Header from './components/Header';
 
-function App() {
+const App: React.FC = () => {
   React.useEffect(() => {
-    window.scrollTo({ top: 0})
-  }, [])
+    window.scrollTo({ top: 0 });
+  }, []);
 
   return (
     <>
@@ -17,21 +17,21 @@ function App() {
 
       <Header />
 
-      <Main style={{ height: '1610vh'}}>
-        <Section style={{ height: '72.7%'}}>
+      <Main style={{ height: '1610vh' }}>
+        <Section style={{ height: '72.7%' }}>
           <FirstAndSecond />
         </Section>
 
-        <Section style={{ height: '9,7%'}}>
-          <Sticky className="third"/>
+        <Section style={{ height: '9,7%' }}>
+          <Sticky className="third" />
         </Section>
 
-        <Section style={{ height: '10.1%'}}>
-          <Sticky className="fourth"/>
+        <Section style={{ height: '10.1%' }}>
+          <Sticky className="fourth" />
         </Section>
       </Main>
     </>
   );
-}
+};
 
 export default App;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,16 @@
-import { useTransform, useViewportScroll } from 'framer-motion';
 import React from 'react';
+import { useTransform, useViewportScroll } from 'framer-motion';
 
 import { Container } from './styles';
-
 
 const Header: React.FC = () => {
   const { scrollYProgress } = useViewportScroll();
 
-  const headerY = useTransform(scrollYProgress, [0.188, 0.198], ['0%', '-100%']);
+  const headerY = useTransform(
+    scrollYProgress,
+    [0.188, 0.198],
+    ['0%', '-100%'],
+  );
 
   return (
     <Container style={{ y: headerY }}>
