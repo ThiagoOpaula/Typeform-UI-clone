af523aa3949083c1605be8cfbe76e646c624d2a1
Thiago Oliveira de Paula <thiago-oliveira2001@live.com>
2020-12-07 18:37:10 -0300

    chore

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,10 +3,11 @@ import styled from 'styled-components';
 
 export const Container = styled(motion.header)`
   width: 100%;
-  height: 100px;
+  height: 90px;
   background: rgba(0, 0, 0, 0.1);
+  align-content: center;
 
   position: fixed;
-  top: 0;
+
   z-index: 99999;
 `;
